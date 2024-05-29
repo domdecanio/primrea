9eb65a81f49441c9d22d7ddf767576483992b5fc
Dominick DeCanio <dominick.c.decanio@pnnl.gov>
2024-05-29 16:35:24 -0700

    Added resources used to troubleshoot python code packaging to repository under folder in root dir. The contents reflect my desktop at the time of completion of the troubleshooting - contents of test_package reflect working version.

---
+++ packaging testing resources/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='test_package',
+      version='0.1',
+      description='Access to PRIMRE knowledge hub metadata and content.',
+#      url='http://github.com/storborg/funniest',
+      author='Dominick DeCanio',
+      author_email='dominick.c.decanio@pnnl.gov'),
+#      license='',
+      packages=['pandas'])
